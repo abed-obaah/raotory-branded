104a2547cee68cba572fa829452f0d71acfb3169
Benjamin Nneji <nnejiben@gmail.com>
2025-01-31 18:58:42 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ export default function Navbar() {
 
         {/* Company logo */}
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <img
               alt=""
