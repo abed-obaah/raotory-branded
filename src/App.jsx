9bbb03c13d42cfa820d5193089227a026f546e51
Benjamin Nneji <nnejiben@gmail.com>
2025-02-01 17:50:46 +0100

    update auth pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Dashboard from "./pages/Dashboard";
+import Auth from "./pages/Auth";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/auth" element={<Auth />} />
       </Routes>
       <Footer />
     </Router>
---
+++ src/pages/Auth/Auth.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../../components/Navbar";
+import Register from "./Register";
+
+export default function Auth() {
+    return (
+        <main>
+            {/* Navbar section */}
+            <Navbar />
+
+            {/* Register - temp */}
+            <Register />
+        </main>
+    )
+}
