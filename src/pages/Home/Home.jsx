d0ad68d0d860c31fbf2f780f6d3ecf81cd613705
Benjamin Nneji <nnejiben@gmail.com>
2025-02-03 18:01:53 +0100

    make home hero responsive

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -16,7 +16,7 @@ export default function Home() {
             </div>
 
             {/* Hero section */}
-            {/* <HomeHero /> */}
+            <HomeHero />
 
             {/* Mockup section */}
             {/* <HomeMockup /> */}
--- src/pages/Home/HomeHero.jsx
+++ src/pages/Home/HomeHero.jsx
@@ -3,9 +3,9 @@ import ButtonRoundedSolid from '../../components/ButtonRoundedSolid'
 export default function HomeHero() {
 
   return (
-    <section className="max-w-6xl mx-auto mt-24">
+    <section className="section-px section-mt max-w-6xl mx-auto">
         <div className="text-center">
-            <h1 className="text-[4rem] font-semibold tracking-tight text-balance text-gray-900 leading-20">
+            <h1 className="text-5xl md:text-[4rem] font-semibold tracking-tight text-balance text-gray-900 md:leading-20">
                 <span className='text-gray-600'>Manage Inventory Like a Pro:</span> From Stock to Sales, <span className='text-gray-600'>All in One Place</span>
             </h1>
             <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8 max-w-[648px] mx-auto">
