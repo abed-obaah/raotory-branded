63ba5b503bd4e10da9f37d256424cc5f54dc3d87
Benjamin Nneji <nnejiben@gmail.com>
2025-02-13 14:48:06 +0100

    update home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+// import Home from "./pages/home";
+import Home from "./pages/home/Home"; // Explicit import of the actual file
+
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Account from "./pages/account";
