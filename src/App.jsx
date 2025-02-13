22b5a81501227ea4d57f23b9a0e6022fb6e4e264
Benjamin Nneji <nnejiben@gmail.com>
2025-02-13 15:04:55 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Home from "./pages/home";
-import Home from "./pages/home/Home"; // Explicit import of the actual file
+// import Home from "./pages/home/Home";
+import Home from "./pages/home/Home.jsx";
 
 import About from "./pages/about";
 import Contact from "./pages/contact";
